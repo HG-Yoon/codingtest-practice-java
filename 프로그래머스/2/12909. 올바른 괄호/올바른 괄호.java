6667e56858cb16fa8dc5240e339e67f251e095ae
hagyeom <younha3710@naver.com>
2025-05-06 13:31:13 +0900

    [level 2] Title: 올바른 괄호, Time: 6.21 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+            boolean solution(String s) {
+            int count = 0;
+
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    count++;
+                } else {
+                    count--;
+                    if (count < 0) return false; // 닫는 괄호가 더 많으면 바로 false
+                }
+            }
+
+            return count == 0; // 다 돌고 0이면 짝 맞음
+        }
+}
