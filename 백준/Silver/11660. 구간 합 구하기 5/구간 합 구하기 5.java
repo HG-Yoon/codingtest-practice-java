46a1d8766e8a5cc9e62312abb84d0090aa896c23
hagyeom <younha3710@naver.com>
2025-05-11 13:57:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 796 ms, Memory: 129968 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,36 +3,39 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 표의 크기 N, 합을 구해야 하는 횟수 M
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        // 원본 배열 입력받기
         int[][] A = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        // 구간 합 배열 생성
         int[][] D = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
             }
         }
-        for (int i = 0; i < M; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int x1 = Integer.parseInt(stringTokenizer.nextToken());
-            int y1 = Integer.parseInt(stringTokenizer.nextToken());
-            int x2 = Integer.parseInt(stringTokenizer.nextToken());
-            int y2 = Integer.parseInt(stringTokenizer.nextToken());
+        // (x1, y1)부터 (x2, y2)의 합을 구해 출력
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
             int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-            bufferedWriter.write(String.valueOf(result) + "\n");
+            bw.write(String.valueOf(result) + "\n");
         }
-        bufferedWriter.flush();
-        bufferedWriter.close();
-        bufferedReader.close();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
